a43c88541a948ec4cb812c35b9137523739805e1
Mike Nichols <mikenichols410@gmail.com>
2019-07-15 02:39:16 -0400

    integrated firebase, created sign-in page with sign-in and out component using Google

--- src/App.js
+++ src/App.js
@@ -6,18 +6,36 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInComponent from './components/sign-up/sign-up.component'
+import { auth } from './firebase/firebase.utills';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => this.setState({ currentUser: user }))
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
     <div>
-      <Header />
+      <Header currentUser={this.state.currentUser} />
       <Switch>    
         <Route exact path='/' component={HomePage}/>
         <Route path='/signin' component={SignInComponent}/>
         <Route path='/shop' component={ShopPage}/>        
       </Switch>
     </div>
-  )
+  )}
 }
 
 export default App;
